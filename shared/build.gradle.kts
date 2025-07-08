999608166eb4247e649da033b2b5267f436014ee
yet <ghg00rus300@gmail.com>
2025-07-08 17:11:38 +0400

    feat: Integrate SKIE plugin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("co.touchlab.skie") version "0.10.4"
 }
 
 kotlin {
